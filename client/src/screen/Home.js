13fe48c704d5bc2beae1a4eb496e48867145a54a
Mohit Singh Rana <mohitsinghrana_2k20b5_18@dtu.ac.in>
2022-04-23 19:08:21 +0530

    db error

--- client/src/screen/Home.js
+++ client/src/screen/Home.js
@@ -1,26 +1,46 @@
-import React from "react";
-import Pizza from "../components/Pizza";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllPizzas } from "../actions/pizzaActions";
 import pizzas from "../pizza_data";
+import Pizza from "../components/Pizza";
+export default function Homescreen() {
+  const dispatch = useDispatch();
 
-export default function Home() {
+  const pizzasstate = useSelector((state) => state.getAllPizzasReducer);
+
+  const { pizz, error, loading } = pizzasstate;
+
+  useEffect(() => {
+    dispatch(getAllPizzas());
+  }, []);
+
   return (
-    <div
-    // style={{
-    //   backgroundImage: url(
-    //     "https://i.pinimg.com/originals/ed/6a/b6/ed6ab6d14143e648da3dc209bc204ad3.jpg"
-    //   ),
-    // }}
-    >
-      <div className="row">
-        {pizzas.map((pizza) => {
-          return (
-            <div className="col-md-4 p-5">
-              <div>
-                <Pizza pizza={pizza} />
+    <div>
+      <div className="row justify-content-center">
+        {loading ? (
+          <h1>loading</h1>
+        ) : error ? (
+          <h1>error</h1>
+        ) : (
+          pizzas.map((pizza) => {
+            return (
+              <div className="col-md-3 m-3" key={pizza._id}>
+                <div>
+                  <Pizza pizza={pizza} />
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })
+        )}
+       { pizzas.map((pizza) => {
+            return (
+              <div className="col-md-3 m-3" key={pizza._id}>
+                <div>
+                  <Pizza pizza={pizza} />
+                </div>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
